03badbce8f58be4482dac831581146217d7bfcc7
Peter Fry <peter@pcfry.com>
2017-01-06 13:14:38 -0800

    Fix warning in Testing

--- project/Testing.scala
+++ project/Testing.scala
@@ -17,34 +17,32 @@ object Testing {
 // TODO: get scalastyle working again
 //        testScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Test).toTask("").value,
 //        (test in Test) <<= (test in Test) dependsOn testScalastyle,
-        resourceDirectory in testScalastyle := baseDirectory.value / "src/test/resources",
-        testOptions in Test := Seq(Tests.Filter(unitFilter))
-      )
+      resourceDirectory in testScalastyle := baseDirectory.value / "src/test/resources",
+      testOptions in Test := Seq(Tests.Filter(unitFilter))
+    )
 
-  private lazy val itSettings =
-    inConfig(IntegrationTest)(Defaults.testSettings) ++
-      Seq(
-        fork in IntegrationTest := false,
-        logBuffered in IntegrationTest := false,
-        parallelExecution in IntegrationTest := false,
-        testOptions in IntegrationTest := Seq(Tests.Filter(itFilter)),
-        resourceDirectory in IntegrationTest := baseDirectory.value / "src/test/resources",
-        scalaSource in IntegrationTest := baseDirectory.value / "src/test/scala")
+  private lazy val itSettings = inConfig(IntegrationTest)(Defaults.testSettings) ++ Seq(
+    fork in IntegrationTest := false,
+    logBuffered in IntegrationTest := false,
+    parallelExecution in IntegrationTest := false,
+    testOptions in IntegrationTest := Seq(Tests.Filter(itFilter)),
+    resourceDirectory in IntegrationTest := baseDirectory.value / "src/test/resources",
+    scalaSource in IntegrationTest := baseDirectory.value / "src/test/scala"
+  )
 
-  private lazy val benchSettings =
-    inConfig(Benchmark)(Defaults.testSettings) ++
-      Seq(
-        testFrameworks in Benchmark += new TestFramework("org.scalameter.ScalaMeterFramework"),
-        fork in Benchmark := false,
-        logBuffered in Benchmark := false,
-        parallelExecution in Benchmark := false,
-        testOptions in Benchmark := Seq(Tests.Filter(benchFilter)),
-        resourceDirectory in Benchmark := baseDirectory.value / "src/test/resources",
-        scalaSource in Benchmark := baseDirectory.value / "src/test/scala")
+  private lazy val benchSettings = inConfig(Benchmark)(Defaults.testSettings) ++ Seq(
+    testFrameworks in Benchmark += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    fork in Benchmark := false,
+    logBuffered in Benchmark := false,
+    parallelExecution in Benchmark := false,
+    testOptions in Benchmark := Seq(Tests.Filter(benchFilter)),
+    resourceDirectory in Benchmark := baseDirectory.value / "src/test/resources",
+    scalaSource in Benchmark := baseDirectory.value / "src/test/scala"
+  )
 
   lazy val settings = testSettings ++ itSettings ++ benchSettings ++ Seq(
     libraryDependencies ++= Dependencies.testDependencies,
-    testAll <<= (test in Benchmark).dependsOn((test in IntegrationTest).dependsOn(test in Test))
+    testAll := (test in Benchmark).dependsOn((test in IntegrationTest).dependsOn(test in Test)).value
   )
 
   lazy val configs = Seq(
