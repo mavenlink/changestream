0114b725e381107b907445785a851830b12d709c
Peter Fry <peter@pcfry.com>
2017-01-09 15:02:27 -0800

    Temporarily disable currentPosition in control actor

--- src/main/scala/changestream/actors/ControlInterface.scala
+++ src/main/scala/changestream/actors/ControlInterface.scala
@@ -110,7 +110,7 @@ trait   ControlInterface extends HttpService with DefaultJsonProtocol {
       ChangeStream.serverName,
       ChangeStream.clientId,
       ChangeStream.isConnected,
-      ChangeStream.currentPosition.toString,
+      "TODO", //ChangeStream.currentPosition.toString,
       ChangestreamEventDeserializer.getCurrentSequenceNumber,
       MemoryInfo(
         Runtime.getRuntime().totalMemory(),
